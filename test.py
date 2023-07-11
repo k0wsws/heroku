5ea628ec63f116679f97452bc0fbaeec38e86759
kimwoosung <k0wsws@gmail.com>
2023-07-11 14:03:20 +0900

    new file

---
+++ test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 16:16:29 2023
+
+@author: user
+"""
+
+import streamlit as st
+
+
+root='C:\\Users\\user\\Desktop\\파이썬\\투자자별 현황\\dataset\\'
+
+## 화면옵션
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+
+st.write("ACE ETF 대쉬보드 입니다. \n\n ACE ETF 홈페이지: https://www.aceetf.co.kr/")                    

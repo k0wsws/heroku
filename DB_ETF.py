0f4beaf9b08643060198a571f9ca16c71fd56131
k0wsws <77596441+k0wsws@users.noreply.github.com>
2023-07-12 11:18:16 +0900

    Add files via upload

---
+++ DB_ETF.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 13:30:12 2023
+
+@author: user
+"""
+import pyodbc
+import pandas as pd
+
+
+server = 'tcp:192.168.194.229,1433'
+database = 'etf'
+username = 'etf'
+password = 'etf@@5282'
+ 
+
+##DB 접속
+
+def conn():
+    connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    return connection
+
+## sql read
+def read(data,connection):
+    
+    return pd.read_sql(data,connection)
+    
